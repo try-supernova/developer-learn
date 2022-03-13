21f081d44a0519dbd947aea6b30fc9dd87698cd1
its-pablo <pablobooks67@gmail.com>
2022-03-12 20:47:09 -0800

    dd

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,17 +48,9 @@ module.exports = {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
+              label: 'Blog',
+              href: 'https://system41.ga/blog',
+            }
           ],
         },
         {
@@ -70,12 +62,12 @@ module.exports = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/system41/open99-docs',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} System41. Built with Docusaurus.`,
     },
   },
   presets: [
