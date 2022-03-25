ca132e66d84691e2c5a306d752d21e75f507e843
its-pablo <78528552+its-pablo@users.noreply.github.com>
2022-03-24 19:50:24 -0700

    titles argh

--- sidebars.js
+++ sidebars.js
@@ -10,15 +10,18 @@ module.exports = {
       items: [
         {
           type: "doc",
+          title: "Repository",
           id: "repository",
         },
         {
           type: "doc",
+          title: "metadata.json",
           id: "metadata"
         },
         {
           type: "doc",
-          id: "submitting-app"
+          id: "submitting-app",
+          title: "Submitting an app"
         }
       ]
     },
