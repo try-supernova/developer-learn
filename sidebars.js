6ba508d39fe06ff7e29b99e1a10cf117515239a1
its-pablo <78528552+its-pablo@users.noreply.github.com>
2022-03-24 19:41:13 -0700

    Extended sidebar syntax

--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,36 @@
 module.exports = {
-  someSidebar: {
-    "Welcome": ["intro"],
-    "Package Manager": ['repository', 'metadata', 'submitting-app'],
-  },
-  sidebaragain: {
-    "Reference": ['md-guide'],
-  }
+  mySidebar: [
+    {
+      type: "doc",
+      id: "intro"
+    },
+    {
+      type: "category",
+      label: "Package Manager",
+      items: [
+        {
+          type: "doc",
+          id: "repository",
+        },
+        {
+          type: "doc",
+          id: "metadata"
+        },
+        {
+          type: "doc",
+          id: "submitting-app"
+        }
+      ]
+    },
+    {
+      type: "category",
+      label: "Reference",
+      items: [
+        {
+          type: "doc",
+          id: "md-guide"
+        }
+      ]
+    }
+  ]
 };
