d4b5c1f178a808b158b749717e7508633a8f950e
its-pablo <78528552+its-pablo@users.noreply.github.com>
2022-04-20 18:09:30 -0700

    ok fix error code

--- sidebars.js
+++ sidebars.js
@@ -10,18 +10,15 @@ module.exports = {
       items: [
         {
           type: "doc",
-          title: "Repository",
           id: "repository",
         },
         {
           type: "doc",
-          title: "metadata.json",
           id: "metadata"
         },
         {
           type: "doc",
           id: "submitting-app",
-          title: "Submitting an app"
         }
       ]
     },
