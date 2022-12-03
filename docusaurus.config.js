039c7f01ca654794cb7f13ee2b91464ef4c3f5d6
its-pablo <78528552+its-pablo@users.noreply.github.com>
2022-12-03 11:08:27 -0800

    fix edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/system41/open99-docs/edit/master/',
+            'https://github.com/system41/open99-docs/edit/main/',
           routeBasePath: "/",
         },
         theme: {
